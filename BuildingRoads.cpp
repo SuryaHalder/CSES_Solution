e8dbae2994f8b1063de7f13e5ec9b8db2c7439a0
SuryaHalder <hsurya2005@gmail.com>
2025-02-20 01:39:18 +0530

    Sol19feb

---
+++ BuildingRoads.cpp
@@ -0,0 +1,137 @@
+
+/*
+	Author : SURYA HALDER
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long int
+#define ff first
+#define ss second
+#define inf INT_MAX
+#define sz size()
+#define rtn return
+#define rnt return
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define pb push_back
+#define endl '\n'
+#define maxheap priority_queue<int> //max heap
+#define minheap priority_queue <int, vector<int>, greater<int>> //min heap
+#define loop(i,a,b) for(int i=a;i<b;i++)     //for loop from a(included)->b(excluded)
+#define roop(i,b,a) for(int i=b;i>=a;i--)    //rev. for loop from b(included)->a(inluded)
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+
+//==============Modular arithematic==============//
+const int mod=1000000000+7; 
+ll mod_pow(ll a,ll b){
+    if(b==0) return 1;
+    if(b%2){
+        return a* mod_pow(a,b-1) %mod;
+    }
+    ll temp= mod_pow(a,b/2);
+    return temp*temp %mod;
+}
+ll mod_mul(ll a,ll b){
+    return ((a%mod) * (b%mod))%mod;
+}
+ll mod_add(ll a,ll b){
+    return ((a%mod) + (b%mod))%mod;
+}
+ll mod_sub(ll a,ll b){
+    return ((a%mod) - (b%mod))%mod;
+}
+ll mod_inv(ll a,ll b=mod-2){
+    return mod_pow(a,b);
+}
+ll mod_div(ll a,ll b){
+    int invb=mod_inv(b);
+    return mod_mul(a,invb);
+}
+//==============End of Modular Arithematic==============//
+
+//==============i/p and o/p==============//
+template<typename T>
+void input(vector<T>& v) {
+    for (auto& x : v) {
+        cin >> x;
+    }
+}
+template<typename T>
+void output(const vector<T>& v) {
+    for (const auto& x : v) {
+        cout << x << " ";
+    }
+    cout << '\n';
+}
+//==============end of i/p and o/p==============//
+
+//==============sorting==============//
+template<typename T>
+void sortA(vector<T>& v) {
+    sort(v.begin(), v.end());
+}
+
+template<typename T>
+void sortD(vector<T>& v) {
+    sort(v.begin(), v.end(), greater<T>());
+}
+//==============End of sorting==============//
+
+int dr[]={-1,0,1,0}; //up,left,down,right
+int dc[]={0,-1,0,1};
+bool in(int i,int j,int n,int m){
+    rtn (i>=0 && i<n && j>=0 && j<m);
+}
+void dfs(int node,vector<vector<int>>&adj,vector<bool> &vis){
+	vis[node]=true;
+	for(auto it:adj[node]){
+		if(!vis[it]) dfs(it,adj,vis);
+	}
+}
+void solve() {
+    /*Your code here */
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> adj(n+1);
+    for(int i=0;i<m;i++){
+    	int u,v;
+    	cin>>u>>v;
+    	adj[u].pb(v);
+    	adj[v].pb(u);
+    }
+    vector<bool> vis(n+1,0);
+    vector<int> ans;
+    for(int i=1;i<=n;i++){
+    	if(!vis[i]){
+    		ans.pb(i);
+    		dfs(i,adj,vis);
+    	} 
+    }
+    cout<<ans.size()-1<<endl;
+    for(int i=0;i<ans.size()-1;i++){
+    	cout<<ans[i]<<" "<<ans[i+1]<<endl;
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //==============Multi testcase==============//
+    int t=1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    //==============comment till here==============//
+    
+    //==============ATCODER ALERT..don't forget to uncomment below==============//
+    
+    //solve();
+    return 0;
+}
